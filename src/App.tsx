70ef278d9b3beffaa8da6cc0faa6886e1c832d7c
Danil <dinkodanil@gmail.com>
2021-02-19 12:45:55 +0300

    Registration and Login sucess

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import AppRoute from './components/AppRoute'
 
 function App() {
   return (
     <>
       <Navbar />
+      <AppRoute />
     </>
   );
 }
--- src/components/AppRoute.tsx
+++ src/components/AppRoute.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import {Redirect, Switch, Route} from 'react-router-dom'
+import { privateRoutes, publicRoutes } from '../routes'
+import {TRoutes} from '../types'
+import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/consts'
 
 function AppRoute() {
-    const user = false
-    return (
-        <div>
-            
-        </div>
-    )
+    const user: boolean = true
+    return user ?
+        (
+            <Switch>
+                {privateRoutes.map(({path, Component}) => 
+                    <Route path={path} component={Component} exact />
+                )}
+                <Redirect to={CHAT_ROUTE} />
+            </Switch>
+        )
+        :
+        (
+            <Switch>
+                {publicRoutes.map(({path, Component}) => 
+                    <Route path={path} component={Component} />
+                )}
+                <Redirect to={LOGIN_ROUTE} />
+            </Switch>
+        )
 }
 
 export default AppRoute
