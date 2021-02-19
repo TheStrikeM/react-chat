8add9a2d239cf98f8df14969519b173bb131ce1d
Danil <dinkodanil@gmail.com>
2021-02-19 14:40:00 +0300

    Setting Chat.tsx

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useSelector} from 'react-redux'
+import {Container, Grid, TextField, Button} from '@material-ui/core';
 
 function Chat() {
+    const {auth, firestore} = useSelector(({auth}: any) => ({
+        auth: auth.auth,
+        firestore: auth.firestore
+    }))
+    const [ user ] = useAuthState(auth)
+    const [value, setValue] = React.useState("")
+
+    const sendMessage = async () => {
+        console.log(value)
+    }
+
     return (
-        <div>
-            Chat
-        </div>
+        <Container>
+            <Grid
+                justify={'center'}
+                style={{height: window.innerHeight - 50, marginTop: '30px'}}
+            >
+                <div style={{width: '100%', height: '60vh', border: '1px solid gray', overflowY: 'auto'}}>
+                    
+                </div>
+                <Grid
+                        container
+                        direction={'column'}
+                        alignItems={'flex-end'}
+                        style={{width: '100%', marginTop: '30px'}}
+                    >
+                        <TextField value={value} onChange={e => setValue(e.target.value)} fullWidth rowsMax={2} variant={"outlined"}/>
+                        <Button variant={"outlined"}>Отправить</Button>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
