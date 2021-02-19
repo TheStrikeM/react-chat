033b1b3f5e4b3b3e4b765604710eabcbf610136e
Danil <dinkodanil@gmail.com>
2021-02-19 13:01:38 +0300

    Setting navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {AppBar, Button, Grid} from '@material-ui/core'
+import Toolbar from '@material-ui/core/Toolbar'
+import { Link } from 'react-router-dom'
+import {LOGIN_ROUTE} from '../utils/consts'
 
 function Navbar() {
+    const user = true
+
     return (
-        <div>
-            Navbar
-        </div>
+        <AppBar color={'secondary'} position="static">
+            <Toolbar>
+                <Grid container justify={'flex-end'}>
+                    {user ? (
+                        <Button variant={"outlined"}>Выйти</Button>
+                    ) : (
+                        <Link to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Логин</Button>
+                        </Link>
+                    )}
+                </Grid>
+            </Toolbar>
+        </AppBar>
     )
 }
 
